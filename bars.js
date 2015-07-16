fdd947449d8627491587d340ef09d9e35c174f59
Chris <chris.carty@gmail.com>
2015-07-15 22:44:50 -0400

    added d3 to lib folder, added example, started filling out actual library, still needs a lot of work

--- bars.js
+++ bars.js
@@ -1,12 +1,15 @@
 !function() {
 	var bars = {
-    	version: "0.0.1"
+    	version: "0.0.1",
+    	author: "Chris Carty"
 	};
 
 	var svg,
+		data,
 		height = 500,
 		width = 400;
 
+	//Initialize the SVG
 	bars.initialize = function(div){
 
 		svg = d3.select(div).append("svg")
@@ -17,9 +20,22 @@
 	}
 
 
+	//Render the bar chart
 	bars.barChart = function(){
 
+		bars.initialize("body");
+
 		//bar chart code goes here
+		svg.selectAll("rect")
+			.data(data)
+			.enter()
+			.append("rect")
+			.attr("height", function(d){
+				return d;
+			})
+			.attr("width", function(d){
+				return 5;
+			})
 	};
 
 
@@ -28,6 +44,15 @@
 		//line chart code goes here
 	}
 
+	bars.setData = function(dataset){
+		if (!arguments){
+			return data;
+		} else {
+			data = dataset;
+		}
+
+		return bars;
+	}
   	//make objects callable
     if (typeof define === "function" && define.amd) define(bars); else if (typeof module === "object" && module.exports) module.exports = bars;
   this.bars = bars;
