7e0ec6dd3d2b0feaf11a78b57b167cffb1d4f99b
Chris Carty <chriscartydev@gmail.com>
2015-08-26 20:31:10 -0400

    started adding piechart

--- fmBars.js
+++ fmBars.js
@@ -12,6 +12,7 @@
 		xScale, y,
 		height,
 		width,
+		radius,
 		padding = 20,
 		margin = 30,
 		offset = 20;
@@ -196,6 +197,57 @@ fmBars.setMax = function(maxHeight){
 			return fmBars
 
 	}
+
+	//////////////////////
+	////							////
+	////	Pie Chart		////
+	////							////
+	//////////////////////
+
+	fmBars.pieChart = function(selector){
+
+		width = width - margin.left - margin.right;
+		height = height - margin.top - margin.bottom;
+
+		radius = Math.min(width, height ) / 2;
+
+		var arc = d3.svg.arc()
+			.outerRadius( radius - 10)
+			.innerRadius(0);
+
+		var pie = d3.layout.pie()
+			.sort(null)
+			.value(function(d){
+				return d.y;
+			});
+
+			svg.append('g')
+				.attr("transform", "translate(" + width /2 + "," + height / 2 + ")");
+
+			var g = svg.selectAll(".arc")
+				.data(pie(data))
+				.enter().append('g')
+				.attr("class", "arc");
+
+			g.append("path")
+				.attr("d", arc)
+				.style("fill", function(d,i){
+					return color(i)
+				})
+
+			g.append("text")
+				.attr("transform", function(d){
+					return "translate(" + arc.centroid(d) + ")";
+				})
+				.attr("dy", ".35em")
+				.style("text-achor", "middle")
+				.text(function(d){
+					return d.data.x;
+				})
+
+			return fmBars;
+	}
+
 
 
 	//Lets make some Axes
